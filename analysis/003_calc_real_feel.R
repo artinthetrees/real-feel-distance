46f198731c4c6a5fac108dcab6c9467626970688
Andrea Tentner <andreat8@gmail.com>
2021-12-12 22:17:06 -0600

    add real feel calc for young people

--- analysis/003_calc_real_feel.R
+++ analysis/003_calc_real_feel.R
@@ -16,22 +16,32 @@
 # (interaction term between temp and humidity)]
 
 age_dummy <- 1
+# https://www.healthline.com/health/exercise-fitness/average-walking-speed#average-speed-by-age
 age_penalty <- 0.15
 
 # if temp is above ideal environmental temp for humans (70F) then set this dummy to 1
 temp_ref_f <- 70
 temp_abv70F_dummy <- quote(ifelse(tmax >= weathermetrics::fahrenheit.to.celsius(temp_ref_f, round = 2),1,0))
-temp_abv70F_penalty <- 0.005
+temp_abv70F_penalty <- 0.0056 
+temp_abv70F_penalty_young <- 0.0035 
+# 5% decrease in aerobic capacity for every 10 degrees celsius >> 0.5% increase in 'distance' for every 1 degree celsius
 
 dp_ref_f <- 55
 dp_abv55F_dummy <- quote(ifelse(tdmean >= weathermetrics::fahrenheit.to.celsius(dp_ref_f, round = 2),1,0)) 
-dp_abv55F_penalty <- 0.091
+dp_abv55F_penalty <- 0.055
+dp_abv55F_penalty_young <- 0.077
 
 # https://www.sciencedirect.com/science/article/pii/S0160412021004591
+# https://www.sciencedirect.com/science/article/pii/S0160412021004591
 # https://ehjournal.biomedcentral.com/articles/10.1186/s12940-016-0167-3
+# https://ieeexplore.ieee.org/abstract/document/8254354
+# https://www.sciencedirect.com/science/article/pii/S1555415506008889?casa_token=0wRrFTicIZUAAAAA:0hqN1TNm9DyCY-dDsIZkLKtrN2mV9T5gsPsv65QkZrMEPiqvWjjhaVqorG6GRCDvrdhBDsloGg
+# https://www.scielo.br/j/aabc/a/TPZpmfGmztHbBjJVnqJLNBv/?lang=en
+# https://repositorio-aberto.up.pt/bitstream/10216/114841/1/MonteiroCarvalho2013.pdf
 hi_ref_f <- 80
 hi_abv80F_dummy <- quote(ifelse(heat_index >= weathermetrics::fahrenheit.to.celsius(hi_ref_f, round = 2),1,0)) 
 hi_abv80F_penalty <- 0.075
+hi_abv80F_penalty_young <- 0.025
 
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3377942/
 walking_dist <- 1000
@@ -51,6 +61,11 @@ chicago.city.daily_data <-
          dp_dist = dist_to_grocery*eval(dp_abv55F_dummy)*dp_abv55F_penalty*(tdmean - weathermetrics::fahrenheit.to.celsius(dp_ref_f, round = 2)),
          hi_dist = dist_to_grocery*eval(hi_abv80F_dummy)*hi_abv80F_penalty*(heat_index - weathermetrics::fahrenheit.to.celsius(hi_ref_f, round = 2)),
          raw_dist = dist_to_grocery,
+         real_feel_dist_young = 
+           dist_to_grocery + 
+           dist_to_grocery*eval(temp_abv70F_dummy)*temp_abv70F_penalty_young*(tmax - weathermetrics::fahrenheit.to.celsius(temp_ref_f, round = 2)),
+           dist_to_grocery*eval(dp_abv55F_dummy)*dp_abv55F_penalty_young*(tdmean - weathermetrics::fahrenheit.to.celsius(dp_ref_f, round = 2)),
+           dist_to_grocery*eval(hi_abv80F_dummy)*hi_abv80F_penalty_young*(heat_index - weathermetrics::fahrenheit.to.celsius(hi_ref_f, round = 2)),
          real_feel_dist = 
            dist_to_grocery + 
            age_dist +
