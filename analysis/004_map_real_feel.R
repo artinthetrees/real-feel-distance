c3c07577825d3ef8b705bea8968cf981bd125763
Andrea Tentner <andreat8@gmail.com>
2022-12-15 09:15:13 -0600

    add creation of env var maps in C in addition to F; add write maps to file; add write impact df to file; write maps and impact df to file for each of the 6 candidate selected days in intermediate data products folder

--- analysis/004_map_real_feel.R
+++ analysis/004_map_real_feel.R
@@ -28,6 +28,12 @@ for (i in 1:length(unique(real_feel$date))){
 
 my_df <- dfs.list[[4]]
 
+maps_path <- paste0(repository_path,"intermediate_data_products/maps/date_",my_df$date[1],"/")
+
+if (!dir.exists(maps_path)){
+  dir.create(maps_path)
+}
+
 sf::st_crs(my_df)$proj4string
 
 # tmap::tm_shape(my_df) +
@@ -35,25 +41,83 @@ sf::st_crs(my_df)$proj4string
 #   tmap::tm_borders() +
 #   tmap::tm_layout(title = "Natural Breaks Map", title.position = c("right","bottom"))
 
+########################################################
+# map env variables in F
+########################################################
+png(paste0(maps_path,"tmax_F_per_census_tract.png"))
+
 tmap::tm_shape(my_df) +
   tmap::tm_fill("tmax_f",title="Temperature (F)",n=10,style="quantile", palette="Reds")  +
   tmap::tm_borders()
+
+dev.off()
+
+png(paste0(maps_path,"tdmean_F_per_census_tract.png"))
 
 tmap::tm_shape(my_df) +
   tmap::tm_fill("tdmean_f",title="Dew Point Temperature (F)",n=10,style="quantile", palette="Reds")  +
   tmap::tm_borders()
 
+dev.off()
+
+png(paste0(maps_path,"heat_index_F_per_census_tract.png"))
+
 tmap::tm_shape(my_df) +
   tmap::tm_fill("heat_index_f",title="Heat Index (F)",n=10,style="quantile", palette="Reds")  +
   tmap::tm_borders()
+
+dev.off()
+
+########################################################
+# map env variables in C
+########################################################
+png(paste0(maps_path,"tmax_C_per_census_tract.png"))
+
+tmap::tm_shape(my_df) +
+  tmap::tm_fill("tmax",title="Temperature (C)",n=10,style="quantile", palette="Reds")  +
+  tmap::tm_borders()
+
+dev.off()
+
+png(paste0(maps_path,"tdmean_C_per_census_tract.png"))
+
+tmap::tm_shape(my_df) +
+  tmap::tm_fill("tdmean",title="Dew Point Temperature (C)",n=10,style="quantile", palette="Reds")  +
+  tmap::tm_borders()
+
+dev.off()
+
+png(paste0(maps_path,"heat_index_C_per_census_tract.png"))
+
+tmap::tm_shape(my_df) +
+  tmap::tm_fill("heat_index",title="Heat Index (C)",n=10,style="quantile", palette="Reds")  +
+  tmap::tm_borders()
+
+dev.off()
+
+########################################################
+# map prevalence of vulnerability condition
+########################################################
+
+png(paste0(maps_path,"p_older_adult_per_census_tract.png"))
 
 tmap::tm_shape(my_df) +
   tmap::tm_fill("p_older_adult",title="% Older Adult",n=10,style="quantile", palette="Greens")  +
   tmap::tm_borders()
 
+dev.off()
+
+png(paste0(maps_path,"n_older_adult_per_census_tract.png"))
+
 tmap::tm_shape(my_df) +
   tmap::tm_fill("tpop_older_adult",title="# of Older Adults",n=10,style="quantile", palette="Greens")  +
   tmap::tm_borders()
+
+dev.off()
+
+########################################################
+# map newly avoided trips prevalence of vulnerability condition
+########################################################
 
 
 my_df$avoid_trip_cat <- ifelse(my_df$raw_dist > 1000,"avoid trip","take trip")
@@ -70,8 +134,12 @@ my_df$avoid_trip_cat <- as.factor(my_df$avoid_trip_cat)
 
 #pal <- RColorBrewer::brewer.pal(3,"PiYG")
 
+png(paste0(maps_path,"avoided_trips_per_census_tract.png"))
+
 tmap::tm_shape(my_df) +
   tmap::tm_fill("avoid_trip_cat",style="cat",palette=c("pink","red","green")) 
+
+dev.off()
 
 
 
@@ -85,7 +153,13 @@ tmap::tm_shape(my_df) +
 #   tmap::tm_borders() +
 #   tmap::tm_layout(title = "Quantile Map", title.position = c("right","bottom"))
 
+########################################################
+# map distance and real feel distance to food source
+########################################################
+
 pal <- RColorBrewer::brewer.pal(10,"PiYG")
+
+png(paste0(maps_path,"raw_distance_per_census_tract.png"))
 
 tmap::tm_shape(my_df) +
   tmap::tm_polygons(col = "raw_dist", 
@@ -97,6 +171,10 @@ tmap::tm_shape(my_df) +
   tmap::tm_layout(legend.outside = TRUE) + 
   tmap::tm_scale_bar(position=c("left", "bottom"))
 
+dev.off()
+
+png(paste0(maps_path,"real_feel_distance_per_census_tract.png"))
+
 tmap::tm_shape(my_df) +
   tmap::tm_polygons(col = "real_feel_dist", 
                     style = "fixed",
@@ -107,8 +185,10 @@ tmap::tm_shape(my_df) +
   tmap::tm_layout(legend.outside = TRUE) + 
   tmap::tm_scale_bar(position=c("left", "bottom"))
 
+dev.off()
 
-hi <- 
+
+impact_real_feel_df <- 
   my_df %>%
   summarize(tpop = sum(tpop),
             tpop_older_adult = sum(tpop_older_adult),
@@ -130,9 +210,12 @@ hi <-
             p_pop_older_adult_below_poverty_newly_avoid_walk = t_pop_older_adult_below_poverty_newly_avoid_walk/t_pop_older_adult_poverty_reported_newly_avoid_walk,
             p_pop_older_adult_live_alone_newly_avoid_walk = t_pop_older_adult_live_alone_newly_avoid_walk/t_pop_older_adult_living_arrange_reported_newly_avoid_walk
             
-            )
+            ) 
 
+# remove geometry from this df before saving as csv
+impact_real_feel_df <- sf::st_set_geometry(impact_real_feel_df,NULL)
 
+write.csv(impact_real_feel_df,paste0(maps_path,"impact_real_feel_df.csv"))
 
 
 
