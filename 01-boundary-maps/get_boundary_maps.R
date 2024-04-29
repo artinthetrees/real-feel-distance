75948d535817bc9f54f2166bc83b71b006adcce8
Andrea Tentner <tentner-andrea@norc.org>
2024-04-29 16:25:14 -0500

    fixed missing comma

--- 01-boundary-maps/get_boundary_maps.R
+++ 01-boundary-maps/get_boundary_maps.R
@@ -297,7 +297,7 @@ tmap::tm_shape(county.tracts.utm) +
 tmap::tmap_save(county_and_city_tracts_map,
                 #paste(repository_path, "intermediate-data-products/boundary-maps/county_and_city_tracts_map_",state,"_",county,"_",city,"_",year,".png", sep = ""),
                 #paste0(boundary_maps_output_sub_dir,"county_and_city_tracts_map.png"),
-                Gmisc::pathJoin(boundary_maps_output_sub_dir,"county_and_city_tracts_map.png")
+                Gmisc::pathJoin(boundary_maps_output_sub_dir,"county_and_city_tracts_map.png"),
                 units = "in",
                 width = 4,
                 height = 6)
