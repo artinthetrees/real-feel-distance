0806fd72d9158347c18546481a0a30c1c5b1c9b4
Andrea Tentner <tentner-andrea@norc.org>
2025-04-03 14:47:12 -0500

    update the get grocery df to be more generalizable (get lat lon points that are within a specific sf polygon boundary) and move this generalizable fx to utils

--- 2025-refactor/03_get_grocery_df.R
+++ 2025-refactor/03_get_grocery_df.R
@@ -1,15 +1,27 @@
-# start with df that includes one col for latitude and one col for longitude as well as 
+# start with grocery df that includes one col for latitude and one col for longitude as well as 
 
-get_grocery_points <- function(grocery_df,lat_var, lon_var, crs_lonlat, crs_utm, city.2km.buffer.utm){
-    g_df_lonlat <- sf::st_as_sf(grocery_df,coords=c(lon_var,lat_var),crs=crs_lonlat)
-    g_df_utm <- sf::st_transform(g_df_lonlat, crs=crs_utm)
 
-    g_intersects <- sf::st_intersects(g_df_utm,city.2km.buffer.utm)
-    g_within_tf <- sapply(g_intersects, function(x) !is_empty(x))
 
-    g_within_pts_orig <- grocery_df[g_within_tf, ]
-    g_within_pts_lonlat <- g_df_lonlat[g_within_tf, ]
-    g_within_pts_utm <- g_df_utm[g_within_tf, ]
+# get_lonlat_points_within_boundary <- function(points_df,lat_var, lon_var,boundary_map_in_utm,crs_lonlat, crs_utm){
 
-    return(list(grocery_within_orig=g_within_pts_orig,grocery_within_lonlat=g_within_pts_lonlat,grocery_within_utm=g_within_pts_utm))
-}
+#     # points_df is a simple dataframe (NOT an sf object) that minimally includes a column with longitude and a column with latitude;
+#     #    where each row is a lonlat point location (e.g. location of a store) 
+#     # lat_var is the name of the df column with latitude
+#     # lon_var is the name of the df column with longitude
+#     # boundary_map_in_utm is an sf object in utm of your boundary polygon; the function will determine which points in points_df are in this boundary
+#     # crs_lonlat is a string that specifies your crs for lonlat maps
+#     # crs_utm is a string that specifies your crs for utm maps
+    
+
+#     p_df_lonlat <- sf::st_as_sf(points_df,coords=c(lon_var,lat_var),crs=crs_lonlat)
+#     p_df_utm <- sf::st_transform(p_df_lonlat, crs=crs_utm)
+
+#     p_intersects <- sf::st_intersects(p_df_utm,boundary_map_in_utm)
+#     p_within_boolean <- sapply(p_intersects, function(x) !is_empty(x))
+
+#     p_within_orig <- points_df[p_within_boolean, ]
+#     p_within_lonlat <- p_df_lonlat[p_within_boolean, ]
+#     p_within_utm <- p_df_utm[p_within_boolean, ]
+
+#     return(list(points_within_orig=p_within_orig,points_within_lonlat=p_within_lonlat,points_within_utm=p_within_utm))
+# }
