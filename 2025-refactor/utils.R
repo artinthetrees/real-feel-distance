7811ea4f8cebe6a75bbc49016bff2e47e0743566
Andrea Tentner <tentner-andrea@norc.org>
2025-04-03 11:32:13 -0500

    refactored some of the code base on adrf; starting to port

---
+++ 2025-refactor/utils.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+
+get_utm_zone <- function(my_census_api_key,county_string,state_string,year_num){
+    tidycensus::census_api_key(my_census_api_key)
+
+    fips_cd_df <-
+        tidycensus::fips_codes %>%
+        filter(county == county_string & state == state_string)
+
+    county_fips_cd <- sprintf("%03s", (fips_cd_df[1, "county_code"]))
+    state_fips_cd <- sprintf("%02s", fips_cd_df[1, "state_code"])
+
+    county_centroid <- 
+        tigris::counties(state = state_string, resolution = "20m", year = year_num) %>%
+        sf::st_centroid() %>%
+        filter(COUNTYFP == county_fips_cd)
+
+    county_centroid <- 
+        county_centroid %>%
+        mutate(lat = unlist(purrr::map(county_centroid$geometry,2)),
+            lon = unlist(purrr::map(county_centroid$geometry,1))) %>% 
+        sf::st_drop_geometry()
+
+    county_centroid_lon <- county_centroid[1,"lon"] 
+    county_utm_zone <- floor((county_centroid_lon + 180) / 6) + 1
+
+    #-----------------------------------
+    crs_lonlat <- "+proj=longlat +datum=NAD83"
+    #crs_utm <- "+proj=utm +zone=16N +datum=NAD83 +ellps=GRS80"
+    #utm_zone <- ggmap::geocode(city)
+    crs_utm <- paste0("+proj=utm +zone=",as.character(county_utm_zone)," +datum=NAD83")
+
+    return(list(crs_lonlat=crs_lonlat, crs_utm=crs_utm, county_utm_zone=county_utm_zone))
+}
