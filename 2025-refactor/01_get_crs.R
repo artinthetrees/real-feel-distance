c19f5ac745920dec211b5c704c387e70af66c0fd
Andrea Tentner <tentner-andrea@norc.org>
2025-04-03 19:08:39 -0500

    move def of input params and sourcing of secrets and utils to main pipeline script

--- 2025-refactor/01_get_crs.R
+++ 2025-refactor/01_get_crs.R
@@ -1,10 +1,10 @@
-state_string <- "IL"
-county_string <- "Cook County"
-city_string <- "Chicago"
-year_num <- 2021
+# state_string <- "IL"
+# county_string <- "Cook County"
+# city_string <- "Chicago"
+# year_num <- 2021
 
-source("./secrets.R")
-source("./utils.R")
+# source("./secrets.R")
+# source("./utils.R")
 
 crs_data <- get_utm_zone(
     my_census_api_key = my_census_api_key,
