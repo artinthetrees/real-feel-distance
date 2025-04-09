372157b41a570233e8a275b57226cbc99c8f2b88
Andrea Tentner <tentner-andrea@norc.org>
2025-04-09 16:12:11 -0500

    minor clean up

--- 2025-refactor/pipeline.R
+++ 2025-refactor/pipeline.R
@@ -1,18 +1,18 @@
-my_font_size <- 10
-options(tigris_use_cache = TRUE)
-
-#install_census_api_key <- TRUE
-#overwrite_existing_census_api_key <- TRUE
-
+###########################################
 state_string <- "IL"
 county_string <- "Cook County"
 city_string <- "Chicago"
 year_num <- 2021
 
-environmental_data <- c("prism")
-
+###########################################
+crs_datum <- "WGS84" # other option is NAD83; use WGS84 when using dodgr
+###########################################
+my_font_size <- 10
+options(tigris_use_cache = TRUE)
+###########################################
 source("./2025-refactor/secrets.R")
 source("./2025-refactor/utils.R")
+source("./01-b-street-network/utils_dodgr.R")
+source("./2025-refactor/01_get_crs.R")
 
-#source("./2025-refactor/01_get_crs.R")
-#source("./2025-refactor/02_get_boundary_maps.R")
+source("./2025-refactor/02_get_boundary_maps.R")
